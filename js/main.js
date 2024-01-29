6f71ad19f3a2bdbfa53738140b9b91db980d9407
jazib iqbal <m.jazibiqbal@gmail.com>
2024-01-29 16:06:55 +0500

    abc

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
-const tl = gsap.timeline();
+// const tl = gsap.timeline();
 
-tl.to("body", {
-  overflow: "hidden"
-})
-  .to(".preloader .text-container", {
-    duration: .5,
-    opacity: 1,
-    ease: "Power3.easeOut"
-  })
-  .from(".preloader .text-container span", {
-    duration: 1.5,
-    delay: .1,
-    y: 150,
-    skewY: 10,
-    stagger: 0.4,
-    ease: "Power3.easeOut"
-  })
-  .to(".preloader .text-container span", {
-    duration: 1.2,
-    y: 150,
-    skewY: -20,
-    stagger: 0.2,
-    ease: "Power3.easeOut"
-  })
-  .to(".preloader", {
-    duration: 1,
-    height: "0vh",
-    ease: "Power3.easeOut"
-  })
-  .to(
-    "body",
-    {
-      overflow: "auto"
-    },
-    "-=1"
-  )
-  .to(".preloader", {
-    display: "none"
-  });
+// tl.to("body", {
+//   overflow: "hidden"
+// })
+//   .to(".preloader .text-container", {
+//     duration: .5,
+//     opacity: 1,
+//     ease: "Power3.easeOut"
+//   })
+//   .from(".preloader .text-container span", {
+//     duration: 1.5,
+//     delay: .1,
+//     y: 150,
+//     skewY: 10,
+//     stagger: 0.4,
+//     ease: "Power3.easeOut"
+//   })
+//   .to(".preloader .text-container span", {
+//     duration: 1.2,
+//     y: 150,
+//     skewY: -20,
+//     stagger: 0.2,
+//     ease: "Power3.easeOut"
+//   })
+//   .to(".preloader", {
+//     duration: 1,
+//     height: "0vh",
+//     ease: "Power3.easeOut"
+//   })
+//   .to(
+//     "body",
+//     {
+//       overflow: "auto"
+//     },
+//     "-=1"
+//   )
+//   .to(".preloader", {
+//     display: "none"
+//   });
 
 
 
